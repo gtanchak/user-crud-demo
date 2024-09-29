64bc905f12c891b2f7db4ad352f0b62e127f8465
gtanchak <ghtanchak77@gmail.com>
2024-09-29 18:05:46 +0530

    feat: seprate business logix

---
+++ src/pages/user/useUser.js
@@ -0,0 +1,77 @@
+import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { useState } from 'react'
+import {
+    createUser,
+    deleteUser,
+    getUsers,
+    updateUser,
+} from '../../services/user.js'
+
+export const useUser = () => {
+    const queryClient = useQueryClient()
+
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    const [editUser, setEditUser] = useState(null)
+
+    const { data: users, isLoading } = useQuery(['users'], getUsers)
+
+    // Mutation to create a new user
+    const createUserMutation = useMutation(createUser, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(['users'])
+        },
+    })
+
+    // Mutation to update a user
+    const updateUserMutation = useMutation(updateUser, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(['users'])
+        },
+    })
+
+    // Handle form submit
+    const handleFormSubmit = (userData) => {
+        if (userData._id) {
+            // Edit existing user
+            updateUserMutation.mutate(userData)
+        } else {
+            createUserMutation.mutate(userData)
+        }
+        setIsModalOpen(false) // Close modal after submitting
+    }
+
+    // Handle edit button click
+    const handleEditClick = (userId) => {
+        const userToEdit = users.find((user) => user._id === userId)
+        setEditUser(userToEdit)
+        setIsModalOpen(true)
+    }
+
+    // Mutation to delete a user
+    const deleteUserMutation = useMutation(deleteUser, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(['users'])
+        },
+    })
+    // Handle delete button click
+    const handleDeleteClick = (userId) => {
+        deleteUserMutation.mutate(userId)
+    }
+
+    const handleAddUser = () => {
+        setEditUser(null)
+        setIsModalOpen(true)
+    }
+
+    return {
+        handleAddUser,
+        handleDeleteClick,
+        editUser,
+        handleEditClick,
+        handleFormSubmit,
+        isLoading,
+        isModalOpen,
+        setIsModalOpen,
+    }
+}
--- src/pages/user/user.jsx
+++ src/pages/user/user.jsx
@@ -1,73 +1,23 @@
-import { useState } from 'react'
 import UsersTable from './components/UserTable.jsx'
 import { IconArrowLeft } from '@tabler/icons-react'
 import { useNavigate } from 'react-router-dom'
 import UserForm from './components/UserForm.jsx'
-import { useMutation, useQuery, useQueryClient } from 'react-query'
-import {
-    createUser,
-    deleteUser,
-    getUsers,
-    updateUser,
-} from '../../services/user.js'
+
+import { useUser } from './useUser.js'
 
 const UserPage = () => {
-    const queryClient = useQueryClient()
+    const {
+        handleAddUser,
+        handleDeleteClick,
+        handleEditClick,
+        isLoading,
+        isModalOpen,
+        handleFormSubmit,
+        editUser,
+        setIsModalOpen,
+    } = useUser()
 
     const navigate = useNavigate()
-    const [isModalOpen, setIsModalOpen] = useState(false)
-
-    const [editUser, setEditUser] = useState(null)
-
-    const { data: users, isLoading } = useQuery(['users'], getUsers)
-
-    // Mutation to create a new user
-    const createUserMutation = useMutation(createUser, {
-        onSuccess: () => {
-            queryClient.invalidateQueries(['users'])
-        },
-    })
-
-    // Mutation to update a user
-    const updateUserMutation = useMutation(updateUser, {
-        onSuccess: () => {
-            queryClient.invalidateQueries(['users'])
-        },
-    })
-
-    // Handle form submit
-    const handleFormSubmit = (userData) => {
-        if (userData._id) {
-            // Edit existing user
-            updateUserMutation.mutate(userData)
-        } else {
-            createUserMutation.mutate(userData)
-        }
-        setIsModalOpen(false) // Close modal after submitting
-    }
-
-    // Handle edit button click
-    const handleEditClick = (userId) => {
-        const userToEdit = users.find((user) => user._id === userId)
-        setEditUser(userToEdit)
-        setIsModalOpen(true)
-    }
-
-    // Mutation to delete a user
-    const deleteUserMutation = useMutation(deleteUser, {
-        onSuccess: () => {
-            queryClient.invalidateQueries(['users'])
-        },
-    })
-    // Handle delete button click
-    const handleDeleteClick = (userId) => {
-        deleteUserMutation.mutate(userId)
-    }
-
-    const handleAddUser = () => {
-        setEditUser(null)
-        setIsModalOpen(true)
-    }
 
     return (
         <section className="container mx-auto">
